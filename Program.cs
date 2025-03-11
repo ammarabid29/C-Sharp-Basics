577c2c4cfb303fa6f239a5f58da2e3f1175f6615
ammarabid29 <ammarabid2829@gmail.com>
2025-03-11 13:17:46 +0500

    pass by value/ reference keywords in methods

--- Program.cs
+++ Program.cs
@@ -4,6 +4,61 @@
     {
         static void Main()
         {
+            //var name = "Ammar";
+            //Console.WriteLine(name);
+            //Console.WriteLine(name.GetType());
+
+            ////determine datatype on runtime 
+            //dynamic age = 10;
+            //Console.WriteLine(age);
+            //Console.WriteLine(age.GetType());
+
+            //age = "Ten";
+            //Console.WriteLine(age);
+            //Console.WriteLine(age.GetType());
+
+            //MyMethod(5, "Ammar");
+
+            int value1 = 10;
+            PassByValue(value1);
+            Console.WriteLine($"MainValue: {value1}");
+
+            int value2 = 10;
+            PassByReference(ref value2);
+            Console.WriteLine($"MainValue: {value2}");
+
+        }
+
+
+        static void PassByValue(int myValue)
+        {
+            myValue += 5;
+            Console.WriteLine($"MyValue: {myValue}");
+        }
+
+        static void PassByReference(ref int myValue)
+        {
+            myValue += 5;
+            Console.WriteLine($"MyValue: {myValue}");
+        }
+
+        static void MyMethod(int length, string message)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
+
+/*
+namespace CSharpBasics
+{
+    class Program
+    {
+        static void Main()
+        {
             int age;
             Console.Write("Enter your age: ");
             string? input = Console.ReadLine();
@@ -25,7 +80,7 @@
             }
             while (check <= 5);
 
-            string message =(2<4)?"Valid message":"Invalid message";
+            string message = (2 < 4) ? "Valid message" : "Invalid message";
             Console.WriteLine(message);
 
             string? select;
@@ -107,3 +162,154 @@
         }
     }
 }
+*/
+
+/*
+namespace CSharpBasics
+{
+    public class MyApp
+    {
+        public static void Main()
+        {
+            string firstName = "          Ammar         ";
+            firstName = firstName.Trim();
+            string lastName = "Abid";
+
+            string name = $"My name is {firstName} {lastName}";
+            Console.WriteLine(name.Replace("Ammar", "Ali Haider"));
+
+            name = name.ToUpper();
+            Console.WriteLine(name.Contains("ABID"));
+            Console.WriteLine(name.Length);
+
+            string sayHello = "Hello World!";
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Hello", "Greetings");
+            Console.WriteLine(sayHello);
+
+            int a = 2100000000;
+            int b = 2100000000;
+            long c = (long)a + (long)b;
+            Console.WriteLine(c);
+
+            double a1 = 42.1;  // Natural Type
+            float b1 = 38.2F;
+            double c1 = a1 + b1;
+
+            decimal a2 = 42.1M;   // Explicit Type
+            decimal b2 = 38.2M;
+            decimal c2 = a2 + b2;
+            Console.WriteLine(c);
+
+            decimal min = Decimal.MinValue;
+            decimal max = Decimal.MaxValue;
+            Console.WriteLine($"The range of decimal is from {min} to {max}");
+
+            int a3 = 5;
+            int b3 = 6;
+            int c3 = a3 + b3;
+            bool myTest = c > 10;
+            if (a + b > 10)
+            {
+                Console.WriteLine("Greater than 10.");
+            }
+            else
+            {
+                Console.WriteLine("Less than 10.");
+            }
+
+            int counter = 0;
+            do
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+            while (counter < 5);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
+                    Console.WriteLine($"The cell is ({row}, {column})");
+                }
+            }
+
+
+            // array[string]
+            // arrays are fixed in length! can't add or remove anything from array
+            var names = new string[]
+            {
+                "Ali",
+                "Ammar",
+                "Abid",
+            };
+            // make new array to add some thing
+            names = [.. names, "David"];
+
+            foreach (var n in names) // Index 2 included but 5 is not  
+            {
+                Console.WriteLine(n.ToUpper());
+            }
+
+
+
+            // List<string>
+            var names1 = new List<string>
+            {
+                "Ali",
+                "Ammar",
+                "Abid",
+            };
+            names1.Add("Scott");
+            names1.Add("David");
+            Console.WriteLine(names[^1]);  // last item in list
+            foreach (var n1 in names1[2..5]) // from index 2 to 4 (5 is exclusive) 
+            {
+                Console.WriteLine(n1.ToUpper());
+            }
+
+
+
+            var numbers = new List<int> { 43, 12, 34, 86, 65 };
+            Console.WriteLine($"I found 34 at index {numbers.IndexOf(34)}");
+            numbers.Sort();
+            Console.WriteLine($"I found 34 at index {numbers.IndexOf(34)}");
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
+
+            // ------ Language Integrated Query -------
+
+            List<int> scores = [3, 45, 82, 97, 92, 100, 81, 60];
+
+            // query expression
+            IEnumerable<int> scoresQuery = from score in scores
+                                           where score > 80
+                                           orderby score descending
+                                           select score;
+            int scoresCount = scoresQuery.Count();
+
+            // method expression
+            var scoresQuery2 = scores.Where(s => s > 80).OrderByDescending(s => s);
+
+            List<int> myScores = [.. scoresQuery];
+
+            // execute the query
+            foreach (var score in myScores)
+            {
+                Console.WriteLine(score);
+            }
+
+
+        }
+    }
+}
+*/
